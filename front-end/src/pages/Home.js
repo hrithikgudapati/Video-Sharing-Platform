ca675fbd0fdc704ccf3da63485856f46fa560761
hrithikgudapati <hrithikgudapati4@gmail.com>
2023-08-10 16:49:31 +0530

    Photo Added

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,17 +1,20 @@
 import React from 'react';
-
-import Movie from '../component/Movie'; // Import your Movie component
-import '../Styles/movie.css'; // Import your home page styling
+import Movie from '../component/Movie';
+import '../Styles/movie.css';
 
 function Home() {
   return (
-    <div>
-     
-      <div className="home-container">
-        <Movie title="" imageSrc="https://i.ytimg.com/vi/lDVQojLPI4Y/maxresdefault.jpg" />
-       
-        {/* Add more Movie components here */}
-      </div>
+    <div className="home-container">
+      <Movie
+        title="RRR "
+        imageSrc="https://i.ytimg.com/vi/lDVQojLPI4Y/maxresdefault.jpg"
+        date="10 Jan 2020"
+        duration="14min"
+        views="200"
+        publisher="Rajamouli"
+        publisherImageSrc=""
+      />
+      {/* Add more Movie components here */}
     </div>
   );
 }
